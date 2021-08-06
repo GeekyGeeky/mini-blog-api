dc4442588ac39811b7ec05fec89a2a20f57d26b9
geekygeeky <tobyolushola@gmail.com>
2021-08-07 00:35:05 +0100

    Added base skeleton to server and app

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import cors from 'cors';
+import express from 'express';
+import { json, urlencoded } from 'express';
+import morgan from 'morgan';
+
+export const app = express()
+
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+export const start = () => {
+	app.listen(3000, () => {
+		console.log("Server running on port: 3000")
+	})
+}
